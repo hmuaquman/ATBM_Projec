7b8ad9213a5072cde1cac3a3bba2e30408a71171
chanhsol0302 <151858877+chanhsol0302@users.noreply.github.com>
2024-04-10 23:13:53 +0700

    script.sql

--- script.sql
+++ script.sql
@@ -166,3 +166,144 @@ BEGIN
         RAISE_APPLICATION_ERROR(-20001, 'Username or role does not exist');
     END IF;
 END;
+/
+--Stored Procedure tạo role
+CREATE OR REPLACE PROCEDURE SP_CREATEROLE (
+    p_role_name IN VARCHAR2
+) AS
+    v_role_count NUMBER;
+BEGIN
+    
+    -- Kiểm tra xem role đã tồn tại chưa
+    SELECT COUNT(*) INTO v_role_count FROM dba_roles WHERE role = p_role_name;
+    
+    IF v_role_count = 0 THEN
+        -- Nếu role chưa tồn tại, tạo role mới
+        EXECUTE IMMEDIATE 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE';
+        EXECUTE IMMEDIATE 'CREATE ROLE ' || p_role_name;
+        DBMS_OUTPUT.PUT_LINE('Role ' || p_role_name || ' created successfully.');
+        EXECUTE IMMEDIATE 'ALTER SESSION SET "_ORACLE_SCRIPT" = FALSE';
+    ELSE
+        -- Nếu role đã tồn tại, thông báo lỗi
+        RAISE_APPLICATION_ERROR(-20001, 'Role ' || p_role_name || ' already exists.');
+    END IF;
+END SP_CREATEROLE;
+/
+--Stored Procedure drop role
+CREATE OR REPLACE PROCEDURE SP_DROPROLE(
+    p_role IN VARCHAR2
+)    
+AS
+    v_count NUMBER;
+    STRSQL VARCHAR(2000);
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count
+    FROM dba_roles
+    WHERE ROLE = upper(p_role);
+    if v_count > 0 then
+        --DBMS_OUTPUT.PUT_LINE('TEST');
+        STRSQL := 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE';
+        EXECUTE IMMEDIATE(STRSQL);
+        STRSQL := 'DROP ROLE ' || p_role;
+        EXECUTE IMMEDIATE(STRSQL);
+        STRSQL := 'ALTER SESSION SET "_ORACLE_SCRIPT" = FALSE';
+        EXECUTE IMMEDIATE(STRSQL);
+    ELSE
+        RAISE_APPLICATION_ERROR(-20001, 'Role does not exist');
+    END IF;
+END;
+/
+--Stored procedure grant role
+CREATE OR REPLACE PROCEDURE SP_GRANTROLE(
+    p_role IN VARCHAR2,
+    p_username IN VARCHAR2
+)    
+AS
+    v_count_role NUMBER;
+    v_count_user NUMBER;
+    STRSQL VARCHAR(2000);
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count_role
+    FROM dba_roles
+    WHERE ROLE = upper(p_role);
+    
+    if v_count_role < 0 then
+        RAISE_APPLICATION_ERROR(-20001, 'Role does not exist');
+    END IF;
+    
+    SELECT COUNT(*)
+    INTO v_count_user
+    FROM dba_users
+    WHERE USER = upper(p_username);
+    
+    if v_count_user < 0 then
+        RAISE_APPLICATION_ERROR(-20001, 'User does not exist');
+    END IF;
+    
+    STRSQL := 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE';
+        EXECUTE IMMEDIATE(STRSQL);
+        STRSQL := 'GRANT ' || p_role || ' TO ' || p_username;
+        EXECUTE IMMEDIATE(STRSQL);
+        STRSQL := 'ALTER SESSION SET "_ORACLE_SCRIPT" = FALSE';
+        EXECUTE IMMEDIATE(STRSQL);
+END;
+/
+--Stored procedure revoke role
+CREATE OR REPLACE PROCEDURE SP_REVOKEROLE(
+    p_role IN VARCHAR2,
+    p_username IN VARCHAR2
+)    
+AS
+    v_count_role NUMBER;
+    v_count_user NUMBER;
+    v_count NUMBER;
+    STRSQL VARCHAR(2000);
+BEGIN
+    SELECT COUNT(*)
+    INTO v_count_role
+    FROM dba_roles
+    WHERE ROLE = upper(p_role);
+    
+    if v_count_role < 0 then
+        RAISE_APPLICATION_ERROR(-20001, 'Role does not exist');
+    END IF;
+    
+    SELECT COUNT(*)
+    INTO v_count_user
+    FROM dba_users
+    WHERE USER = upper(p_username);
+    
+    if v_count_user < 0 then
+        RAISE_APPLICATION_ERROR(-20001, 'User does not exist');
+    END IF;
+    
+    SELECT COUNT(*)
+    INTO v_count
+    FROM dba_role_privs
+    WHERE GRANTEE = upper(p_username) AND GRANTED_ROLE = upper(p_role);
+    
+    if v_count < 0 then
+        RAISE_APPLICATION_ERROR(-20001, 'This role does not grant to user');
+    END IF;
+    
+    STRSQL := 'ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE';
+        EXECUTE IMMEDIATE(STRSQL);
+        STRSQL := 'REVOKE ' || p_role || ' FROM ' || p_username;
+        EXECUTE IMMEDIATE(STRSQL);
+        STRSQL := 'ALTER SESSION SET "_ORACLE_SCRIPT" = FALSE';
+        EXECUTE IMMEDIATE(STRSQL);
+END;
+/
+--Stored Procedure check role
+create or replace PROCEDURE SP_CHECKROLE
+(p_username in varchar2,
+c2 out sys_refcursor)
+AUTHID CURRENT_USER AS
+    l_check NVARCHAR2(20) :=p_username;
+BEGIN
+    open c2 for
+    SELECT * from DBA_ROLE_PRIVS  where GRANTEE= l_check;
+    --dbms_sql.return_result(c2);
+END;
