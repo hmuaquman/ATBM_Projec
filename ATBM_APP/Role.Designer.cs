befef106cd852756d9deaaf5f93a1faaa4928d7a
Dattrong <trongleviet05@gmail.com>
2024-06-02 11:56:28 +0700

    Change data

--- ATBM_APP/Role.Designer.cs
+++ ATBM_APP/Role.Designer.cs
@@ -45,19 +45,21 @@
             // roleGridView
             // 
             this.roleGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.roleGridView.Location = new System.Drawing.Point(248, 26);
+            this.roleGridView.Location = new System.Drawing.Point(186, 21);
+            this.roleGridView.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.roleGridView.Name = "roleGridView";
             this.roleGridView.RowHeadersWidth = 51;
             this.roleGridView.RowTemplate.Height = 24;
-            this.roleGridView.Size = new System.Drawing.Size(823, 451);
+            this.roleGridView.Size = new System.Drawing.Size(617, 366);
             this.roleGridView.TabIndex = 1;
             // 
             // refreshButton
             // 
             this.refreshButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.refreshButton.Location = new System.Drawing.Point(31, 26);
+            this.refreshButton.Location = new System.Drawing.Point(23, 21);
+            this.refreshButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.refreshButton.Name = "refreshButton";
-            this.refreshButton.Size = new System.Drawing.Size(159, 39);
+            this.refreshButton.Size = new System.Drawing.Size(119, 32);
             this.refreshButton.TabIndex = 2;
             this.refreshButton.Text = "Refresh";
             this.refreshButton.UseVisualStyleBackColor = true;
@@ -67,9 +69,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label2.Location = new System.Drawing.Point(23, 134);
+            this.label2.Location = new System.Drawing.Point(17, 109);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 20);
+            this.label2.Size = new System.Drawing.Size(38, 17);
             this.label2.TabIndex = 9;
             this.label2.Text = "User";
             // 
@@ -77,34 +80,38 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.label1.Location = new System.Drawing.Point(22, 93);
+            this.label1.Location = new System.Drawing.Point(16, 76);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.Size = new System.Drawing.Size(37, 17);
             this.label1.TabIndex = 8;
             this.label1.Text = "Role";
             // 
             // usrTB
             // 
             this.usrTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.usrTB.Location = new System.Drawing.Point(71, 129);
+            this.usrTB.Location = new System.Drawing.Point(53, 105);
+            this.usrTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.usrTB.Name = "usrTB";
-            this.usrTB.Size = new System.Drawing.Size(122, 28);
+            this.usrTB.Size = new System.Drawing.Size(92, 24);
             this.usrTB.TabIndex = 7;
             // 
             // roleTB
             // 
             this.roleTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
-            this.roleTB.Location = new System.Drawing.Point(71, 88);
+            this.roleTB.Location = new System.Drawing.Point(53, 72);
+            this.roleTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.roleTB.Name = "roleTB";
-            this.roleTB.Size = new System.Drawing.Size(122, 28);
+            this.roleTB.Size = new System.Drawing.Size(92, 24);
             this.roleTB.TabIndex = 6;
             // 
             // createRoleButton
             // 
             this.createRoleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.createRoleButton.Location = new System.Drawing.Point(34, 188);
+            this.createRoleButton.Location = new System.Drawing.Point(26, 153);
+            this.createRoleButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.createRoleButton.Name = "createRoleButton";
-            this.createRoleButton.Size = new System.Drawing.Size(159, 39);
+            this.createRoleButton.Size = new System.Drawing.Size(119, 32);
             this.createRoleButton.TabIndex = 11;
             this.createRoleButton.Text = "Create Role";
             this.createRoleButton.UseVisualStyleBackColor = true;
@@ -113,9 +120,10 @@
             // dropRoleButton
             // 
             this.dropRoleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.dropRoleButton.Location = new System.Drawing.Point(34, 251);
+            this.dropRoleButton.Location = new System.Drawing.Point(26, 204);
+            this.dropRoleButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dropRoleButton.Name = "dropRoleButton";
-            this.dropRoleButton.Size = new System.Drawing.Size(159, 39);
+            this.dropRoleButton.Size = new System.Drawing.Size(119, 32);
             this.dropRoleButton.TabIndex = 12;
             this.dropRoleButton.Text = "Drop Role";
             this.dropRoleButton.UseVisualStyleBackColor = true;
@@ -124,9 +132,10 @@
             // grantRoleButton
             // 
             this.grantRoleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.grantRoleButton.Location = new System.Drawing.Point(34, 312);
+            this.grantRoleButton.Location = new System.Drawing.Point(26, 254);
+            this.grantRoleButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.grantRoleButton.Name = "grantRoleButton";
-            this.grantRoleButton.Size = new System.Drawing.Size(159, 39);
+            this.grantRoleButton.Size = new System.Drawing.Size(119, 32);
             this.grantRoleButton.TabIndex = 13;
             this.grantRoleButton.Text = "Grant Role";
             this.grantRoleButton.UseVisualStyleBackColor = true;
@@ -135,9 +144,10 @@
             // revokeRoleButton
             // 
             this.revokeRoleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.revokeRoleButton.Location = new System.Drawing.Point(34, 376);
+            this.revokeRoleButton.Location = new System.Drawing.Point(26, 306);
+            this.revokeRoleButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.revokeRoleButton.Name = "revokeRoleButton";
-            this.revokeRoleButton.Size = new System.Drawing.Size(159, 39);
+            this.revokeRoleButton.Size = new System.Drawing.Size(119, 32);
             this.revokeRoleButton.TabIndex = 14;
             this.revokeRoleButton.Text = "Revoke Role";
             this.revokeRoleButton.UseVisualStyleBackColor = true;
@@ -146,9 +156,10 @@
             // checkRoleButton
             // 
             this.checkRoleButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkRoleButton.Location = new System.Drawing.Point(34, 438);
+            this.checkRoleButton.Location = new System.Drawing.Point(26, 356);
+            this.checkRoleButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.checkRoleButton.Name = "checkRoleButton";
-            this.checkRoleButton.Size = new System.Drawing.Size(159, 39);
+            this.checkRoleButton.Size = new System.Drawing.Size(119, 32);
             this.checkRoleButton.TabIndex = 15;
             this.checkRoleButton.Text = "Check Role";
             this.checkRoleButton.UseVisualStyleBackColor = true;
@@ -156,9 +167,9 @@
             // 
             // Role
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1109, 498);
+            this.ClientSize = new System.Drawing.Size(832, 405);
             this.Controls.Add(this.checkRoleButton);
             this.Controls.Add(this.revokeRoleButton);
             this.Controls.Add(this.grantRoleButton);
@@ -170,6 +181,7 @@
             this.Controls.Add(this.roleTB);
             this.Controls.Add(this.refreshButton);
             this.Controls.Add(this.roleGridView);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Role";
             this.Text = "Role Management";
             this.Load += new System.EventHandler(this.Role_Load);
